3fcb87b2ad15643ba10660e8e64fe0ae49f54455
ravielze <ravielz20@gmail.com>
2020-11-12 17:27:25 +0700

    Init Vezz and Selch

---
+++ server/VS.py
@@ -0,0 +1,21 @@
+class Vezz():
+    """Vezz stands for Vectorizer"""
+
+    def __init__(self):
+        pass
+
+    def dot(self, d: dict, d2: dict) -> float:
+        """Return Similarities"""
+        pass
+
+class Selch():
+    """ Selch stands for Search """
+    
+    def __init__(self):
+        pass
+
+    def search(self, query: str) -> list:
+        pass
+
+    def documentComparing(self, is_bahasa_indonesia: bool, filename: str, filename2: str) -> dict:
+        pass
