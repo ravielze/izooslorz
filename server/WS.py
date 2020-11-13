6e9d1b91c14470bcd09e95b54cf65f15f474daa5
ravielze <ravielz20@gmail.com>
2020-11-13 15:05:57 +0700

    Finish WebScraping

--- server/WS.py
+++ server/WS.py
@@ -13,10 +13,19 @@ import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 import time, string, random
 from LPP import LPP
+from Data import Data
+from BDIter import biiter
 
 class Scraper():
     def __init__(self, lpp: LPP):
         self.__lpp = lpp
+        self.__data = Data('webscraping', ['filename', 'language', 'url'])
+
+    def refresh(self):
+        I = self.__data.readIter()
+        I.next()
+        while (I.hasNext()):
+            now = dict(I.next())
 
     def getPath(self, is_bahasa_indonesia: bool, filename: str) -> str:
         return './documents/' + ('bahasa' if (is_bahasa_indonesia) else 'english')+"/" + filename
@@ -27,19 +36,35 @@ class Scraper():
 
     def htmlScraper(self, is_bahasa_indonesia: bool, url: str) -> bool:
         success = True
+        lang = 'bahasa' if (is_bahasa_indonesia) else 'english'
         try:
-            response = urllib.request.urlopen(url)
-            webContent = response.read()
-            soup = BeautifulSoup(webContent.decode('utf8'), 'html.parser')
-            title = self.__lpp.lemmatize(soup.find('title').string).replace(' ', '')
+            response        = urllib.request.urlopen(url)
+            webContent      = response.read()
+            soup            = BeautifulSoup(webContent.decode('utf8'), 'html.parser')
 
-            f = open(self.getPath(is_bahasa_indonesia, title + "_" + self.randomNaming(9) + ".html"), 'wb')
+            title           = self.__lpp.lemmatize(soup.find('title').string).replace(' ', '')
+            fname           = title + "_" + self.randomNaming(9) + ".html"
+            f               = open(self.getPath(is_bahasa_indonesia, fname), 'wb')
+
             f.write(webContent)
             f.close()
+            self.__data.writenl({'filename': fname, 'language': lang, 'url': url})
         except:
             success = False
         return success
+    
+    def findUrl(self, filename: str, is_bahasa_indonesia: bool) -> str:
+        I = self.__data.readIter_filter(filename, is_bahasa_indonesia)
+        if I.hasNext():
+            now = dict(I.next())
+            return now['url']
+        else:
+            return ""
 
-lpp = LPP()
-s = Scraper(lpp)
-print(s.htmlScraper(False, 'https://janav.wordpress.com/2013/10/27/tf-idf-and-cosine-similarity/'))
+    def getDocuments(self, is_bahasa_indonesia: bool) -> list:
+        result = []
+        I = biiter(self.__data.readIter_filter(None, is_bahasa_indonesia).getList())
+        while (I.hasNext()):
+            now = dict(I.next())
+            result.append(now['filename'])
+        return result
