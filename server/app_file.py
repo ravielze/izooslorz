312e4f792943712911a436cb7885890232db2544
ravielze <ravielz20@gmail.com>
2020-11-14 17:10:21 +0700

    Threaded File Processing

--- server/app_file.py
+++ server/app_file.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, redirect, url_for, jsonify, send_from_directory
+from flask import Blueprint, request, jsonify, send_from_directory
 import time
 import os
+from threading import Thread
 from werkzeug.utils import secure_filename
 from DM import TEXTRACT_EXT
 from LPP import LPP
@@ -14,10 +15,10 @@ ALLOWED_EXT = set(TEXTRACT_EXT.copy())
 
 lpp = LPP()
 dm = DM(lpp)
-tf = TF(dm, lpp)
-tf.refresh(None)
-idf = IDF(dm, lpp)
-idf.refresh(None)
+tf = TF(dm)
+tf.refreshAll()
+idf = IDF(dm)
+idf.refreshAll()
 ss = Selch(dm, lpp)
 
 def allowed_file(filename):
@@ -38,8 +39,17 @@ def englishfile(filename):
 fileapp.add_url_rule('/file/bahasa/<filename>', 'bahasafile', build_only=True)
 fileapp.add_url_rule('/file/english/<filename>', 'englishfile', build_only=True)
 
+cache = {'IS_READING': False}
+
 @fileapp.route('/upload', methods=['GET', 'POST'])
 def uf():
+
+    def process(lang: bool, filename: str):
+        dm.read(lang, filename)
+        tf.process(lang, filename)
+        idf.process(lang, filename)
+        print(f"{filename} Done.")
+        
     """ {lang: bahasa_indonesia/english, file:[file]}"""
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -51,12 +61,12 @@ def uf():
             return jsonify({'message': 'File not found.'})
         if request.form.get('lang') in ['bahasa_indonesia', 'english']:
             if file and allowed_file(file.filename):
+                cache['IS_READING'] = True
                 filename = str(int(time.time())) + "_" + secure_filename(file.filename)
                 folder = BAHASA_FOLDER if (request.form.get('lang') == 'bahasa_indonesia') else ENGLISH_FOLDER
                 file.save(os.path.join(folder, filename))
-                dm.read((request.form.get('lang') == 'bahasa_indonesia'), filename)
-                tf.process((request.form.get('lang') == 'bahasa_indonesia'), filename)
-                idf.refresh((request.form.get('lang') == 'bahasa_indonesia'))
+                thread = Thread(target=process, kwargs={'filename': filename, 'lang': (request.form.get('lang') == 'bahasa_indonesia')})
+                thread.start()
                 return jsonify({'message': 'Success.'})
         else:
             return jsonify({'message': 'Language not found.'})
