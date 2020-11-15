a07879c01ac1dbf4d37adc270c85743eb23ad96b
kinantanbagaspati <kinantanyolo@gmail.com>
2020-11-15 21:37:50 +0700

    About Us Page

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -11,54 +11,67 @@ const AboutUsPage = () => {
   return (
     <div>
       <NavBar />
-      <br></br>
-      <br></br>
-      <br></br>
-      <img src={Image}></img>
-      <p>
-        <h2>How to Use</h2>
-        Uploadlah dokumen terlebih dahulu. Proses upload dokumen dapat dilakukan
-        dengan mengklik tombol upload pada home page. Tombol tersebut akan
-        menredirect user ke halaman upload. Tekanlah tombol choose file dan
-        pilihlah file yang akan diupload. Cara upload juga bisa dilakukan dengan
-        cara drag and drop. Setelah memastikan file yang terpilih benar, tekan
-        tombol upload maka dokumen akan secara otomatis tersimpan dan siap untuk
-        digunakan. Tekanlah tombol back untuk kembali ke home page. Ketiklah
-        query pada search bar, dan tekan enter untuk melakukan search.
+      <div className="Container">
         <br></br>
-        <br></br>
+        <div className="HowToUse">
+          <img src={Image}></img>
+          <div className="HowToUseRight">
+            <h2>How to Use</h2>
+            <p>
+              Uploadlah dokumen terlebih dahulu. Proses upload dokumen dapat dilakukan
+              dengan mengklik tombol upload pada home page. Tombol tersebut akan
+              menredirect user ke halaman upload. Tekanlah tombol choose file dan
+              pilihlah file yang akan diupload. Cara upload juga bisa dilakukan dengan
+              cara drag and drop. Setelah memastikan file yang terpilih benar, tekan
+              tombol upload maka dokumen akan secara otomatis tersimpan dan siap untuk
+              digunakan. Tekanlah tombol back untuk kembali ke home page. Ketiklah
+              query pada search bar, dan tekan enter untuk melakukan search.
+            </p>
+          </div>
+          <br></br>
+        </div>
         <h2>About Us</h2>
-        Pembuat website ini merupakan mahasiswa Teknik Informatika Institut
-        Teknologi Bandung angkatan 2019, di antaranya :
+        <div className="developers">
+          <div className="member">
+            <img src={Viel} style={{width:200}}></img>
+            <div className="memberRight">
+              <div className="memberName">
+                <p style={{fontSize:20}}>Steven Nataniel Kodyat </p>
+              </div>
+              <p>13519002 </p>
+              <p>Teknik Informatika ITB</p>
+              <p>Jakarta</p>
+              <p>Motto : </p>
+            </div>
+          </div>
+          <div className="member">
+            <img src={Kinan} style={{width:200}}></img>
+            <div className="memberRight">
+              <div className="memberName">
+                <p style={{fontSize:20}}>Kinantan Arya Bagaspati</p>
+              </div>
+              <p>13519044</p>
+              <p>Teknik Informatika ITB</p>
+              <p>Purwokerto</p>
+              <p>Selama janur kuning belum melengkung,</p>
+              <p>masih bisa ditikung.</p>
+            </div>
+          </div>
+          <div className="member">
+            <img src={Kaer} style={{width:200}}></img>
+            <div className="memberRight">
+              <div className="memberName">  
+                <p style={{fontSize:20}}>Kevin Ryan</p>
+              </div>
+              <p>13519191</p>
+              <p>Teknik Informatika ITB</p>
+              <p>Medan</p>
+              <p>Motto : Wubbalubbadubdub</p>
+            </div>
+          </div>
+        </div>
         <br></br>
-        <img src={Viel}></img>
-        <br></br>Nama : Steven Nataniel Kodyat 
-        <br></br>NIM : 13519002
-        <br></br>Jurusan : Teknik Informatika
-        <br></br>Asal Universitas : Institut Teknologi Bandung
-        <br></br>Asal Daerah : Jakarta
-        <br></br>Motto : 
-        <br></br>
-        <br></br>
-        <img src={Kinan}></img>
-        <br></br>Nama : Kinantan Arya Bagaspati
-        <br></br>NIM : 13519044
-        <br></br>Jurusan : Teknik Informatika
-        <br></br>Asal Universitas : Institut Teknologi Bandung
-        <br></br>Asal Daerah : Purwokerto
-        <br></br>Motto : Selama janur kuning belum melengkung, masih bisa ditikung.
-        <br></br>Perkuat pusat perbanyak cabang.
-        <br></br>
-        <br></br>
-        <img src={Kaer}></img>
-        <br></br>Nama : Kevin Ryan
-        <br></br>NIM : 13519191
-        <br></br>Jurusan : Teknik Informatika
-        <br></br>Asal Universitas : Institut Teknologi Bandung
-        <br></br>Asal Daerah : Medan
-        <br></br>Motto : Wubbalubbadubdub
-      </p>
-      <br></br>
+      </div>
     </div>
   );
 };
