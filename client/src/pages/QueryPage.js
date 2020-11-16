0fc10504f1fd87a94fc25a7722186cadf0ea73b8
kinantanbagaspati <kinantanyolo@gmail.com>
2020-11-16 08:40:14 +0700

    Benerin error

--- client/src/pages/QueryPage.js
+++ client/src/pages/QueryPage.js
@@ -1,41 +1,58 @@
 import "../App.css";
-import React, {useState} from "react";
+import React from 'react';
+import axios from 'axios';
 import DocuQuery from "../components/Documents";
-import NavBar from "../components/NavBar.js";
 import Table from "../components/TermTable";
 import { useEffect } from "react";
-import axios from "axios";
+import NavBar from "../components/NavBar.js";
+import SearchBar from "../components/SearchBar.js";
+import SearchIcon from "@material-ui/icons/Search";
+import Image from "../img/logo.jpg";
 
-const lang = {
-    "bahasa_indonesia" : "id",
-    "english" : "en"
-}
+export default class QueryPage extends React.Component {
+  state = {
+    name: this.props.location.query,
+    data: [],
+    termtable: [{documents: []}],
+  }
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
 
-const QueryPage = (props) => {
-  const [data,setData] = useState([]);
-  useEffect(() => {
-      const lang_converted = lang[props.match.params.lang]
-      const search = props.match.params.query
-    axios
-    .post(`localhost:5000/search`, {"keyword": search, "lang": lang_converted})
-    .then((res) => {
-        res = res.data.data
-        setData(res)
-    })
-    console.log(data);
+  handleSubmit = event => {
+    event.preventDefault();
 
-  });
-  return (
-    <div>
-      <NavBar />
-      <div className="Container">
-        <div className="QueryPage-Head"></div>
-        <div className="QueryPage-Body">
-          <DocuQuery data={data} />
-          <Table />
+    const search = {
+      'keyword': this.state.name,
+      'lang': "en"
+    };
+    axios.post(`http://localhost:5000/search`, search)
+      .then(res => {
+        console.log(res.data.data);
+        this.setState({data: res.data.data});
+        this.setState({termtable: res.data.termtable})
+      })
+  }
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="Container">
+          <div className="QueryPage-Head">
+            <img src={Image} class="App-logo" alt="" style={{width: 200, marginRight: 50}}></img>
+            <form onSubmit={this.handleSubmit} style={{display: "flex", justifyContent: "row", alignItems: "center"}}>
+              <input type="text" name="name" onChange={this.handleChange} style={{height: 40, borderRadius: 10}}/>
+              <button type="submit" style={{height: 40}}>
+                <SearchIcon />
+              </button>
+            </form>
+          </div>
+          <div className="QueryPage-Body">
+            <DocuQuery data = {this.state.data}/>
+            <Table data = {this.state.termtable}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-export default QueryPage;
+    )
+  }
+}
